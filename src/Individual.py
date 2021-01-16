ede84619de066ced4ca3f80ce4cad3a56490de38
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-01-16 22:59:34 +0900

    create Individual

---
+++ src/Individual.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class Individual:
+  def __init__(self, individual_size):
+    self.gene = f'{np.random.randint(2**individual_size):0{individual_size}b}'
+    self.fitness = sum([int(i) for i in self.gene])
+    self.individual_size = individual_size
+
+  def evaluation(self):
+    self.fitness = sum([int(i) for i in self.gene])
+
+
+if __name__ == '__main__':
+  ind_length = 5
+  ind1 = Individual(ind_length)
+  print(ind1.gene)
+  print(ind1.fitness)
+  
