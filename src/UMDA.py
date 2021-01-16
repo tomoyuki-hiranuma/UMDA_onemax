2154796c419ce12b96ea299dd6d99178d042932e
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-01-17 00:42:21 +0900

    plot png

--- src/UMDA.py
+++ src/UMDA.py
@@ -1,6 +1,7 @@
 import numpy as np
 from Population import Population
 import copy
+import matplotlib.pyplot as plt
 
 class Umda:
   def __init__(self, tau, population_size, individual_size):
@@ -26,14 +27,31 @@ if __name__ == '__main__':
   TAU = 0.5
   POPULATION_SIZE = 100
   INDIVIDUAL_SIZE = 50
+  MAX_GENERATION = 50
 
+  vector_entries = []
+  steps = []
   umda = Umda(TAU, POPULATION_SIZE, INDIVIDUAL_SIZE)
+  vector_entries.append(umda.probability_array)
+  steps.append(0)
   print("第{}世代".format(0))
   umda.population.print_population()
-  for i in range(50):
+  for i in range(MAX_GENERATION):
     print("第{}世代".format(i+1))
     umda.do_one_generation()
     umda.population.print_population()
+    vector_entries.append(umda.probability_array)
+    steps.append(i+1)
+
+  plt.plot(steps, vector_entries, color="blue")
+  plt.ylim(0, 1.1)
+  plt.xlim(0, MAX_GENERATION)
+  plt.xlabel("generation")
+  plt.ylabel("probability vector entries")
+  plt.title("probability vector entries in each generation")
+  plt.savefig("./png/UMDA_N={}_n={}_tau={}.png".format(POPULATION_SIZE, INDIVIDUAL_SIZE, TAU))
+  # plt.show()
+
 
 
 
